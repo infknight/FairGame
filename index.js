a5d0871ca10e3d7ba95024fba0af6622fabe6e57
Jason <jasonjia@tamu.edu>
2021-02-15 20:32:32 -0600

    write javascript about random game process

---
+++ index.js
@@ -0,0 +1,25 @@
+var rand1 = Math.random(); 
+rand1 = Math.floor(rand1 * 3) + 1; 
+
+
+
+// 1 is rock 2 is paper 3 is scissors 
+
+if(rand1 === 1){
+    document.querySelector(".game1").setAttribute("src","images/rock.png");
+} else if(rand1 === 2){
+    document.querySelector(".game1").setAttribute("src","images/paper.png");
+} else if(rand1 === 3){
+    document.querySelector(".game1").setAttribute("src","images/scissors.png")
+}
+
+var rand2 = Math.random();
+rand2 = Math.floor(rand2 * 3) + 1; 
+
+if (rand2 === 1){
+    document.querySelector(".game2").setAttribute("src","images/rock.png");
+} else if(rand2 === 2){
+    document.querySelector(".game2").setAttribute("src","images/paper.png");
+} else if(rand2 === 3){
+    document.querySelector(".game2").setAttribute("src","images/scissors.png")
+} 
