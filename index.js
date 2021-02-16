d7a7e6c90d842ef00a2a5858bca14bd3a1c4d7a5
Jason <jasonjia@tamu.edu>
2021-02-16 09:49:53 -0600

    added winning codition for js

--- index.js
+++ index.js
@@ -22,6 +22,30 @@ function rpsgame(){
     } else if(rand2 === 3){
         document.querySelector(".game2").setAttribute("src","images/scissors.png")
     } 
+
+    if(rand1 == rand2){
+        document.querySelector("h1").innerText = "IT IS A DRAW"; 
+    }
+
+    if(rand1 === 1 && rand2 == 2){
+        document.querySelector("h1").innerHTML = "PLAYER 2 WON"; 
+    }
+    if(rand1 == 1 && rand2 == 3){
+        document.querySelector("h1").innerHTML = "PLAYER 1 WON"; 
+    }
+    if(rand1 == 2 && rand2 == 1){
+        document.querySelector("h1").innerHTML = "PLAYER 1 WON"; 
+    }
+    if(rand1 == 2 && rand2 == 3){
+        document.querySelector("h1").innerHTML = "PLAYER 2 WON"; 
+    }
+    if(rand1 == 3 && rand2 == 1){
+        document.querySelector("h1").innerHTML = "PLAYER 2 WON"; 
+    }
+    if(rand1 == 3 && rand2 == 2){
+        document.querySelector("h1").innerHTML = "PLAYER 1 WON"; 
+    }
+
 }
 
 
