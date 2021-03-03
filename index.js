04355db135cbdce5416d3b63cfc7f4147ab48812
Jason <jasonjia@tamu.edu>
2021-03-03 16:14:08 -0600

    updated if statement for js. Adding counter for player one and two

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // 1 is rock 2 is paper 3 is scissors 
+
+var count_one = 0; 
+var count_two = 0; 
+
+
 function rpsgame(){
     var rand1 = Math.random(); 
     rand1 = Math.floor(rand1 * 3) + 1; 
@@ -27,24 +32,47 @@ function rpsgame(){
         document.querySelector("h1").innerText = "IT IS A DRAW"; 
     }
 
+
+    var win1 = false; 
+    var win2 = false; 
+
     if(rand1 === 1 && rand2 == 2){
-        document.querySelector("h1").innerHTML = "PLAYER 2 WON"; 
+        win2 = true; 
+        // document.querySelector("h1").innerHTML = "PLAYER 2 WON"; 
     }
     if(rand1 == 1 && rand2 == 3){
-        document.querySelector("h1").innerHTML = "PLAYER 1 WON"; 
+        win1 = true; 
+        // document.querySelector("h1").innerHTML = "PLAYER 1 WON"; 
     }
     if(rand1 == 2 && rand2 == 1){
-        document.querySelector("h1").innerHTML = "PLAYER 1 WON"; 
+        win1 = true; 
+        // document.querySelector("h1").innerHTML = "PLAYER 1 WON"; 
     }
     if(rand1 == 2 && rand2 == 3){
-        document.querySelector("h1").innerHTML = "PLAYER 2 WON"; 
+        win2 = true; 
+        // document.querySelector("h1").innerHTML = "PLAYER 2 WON"; 
     }
     if(rand1 == 3 && rand2 == 1){
-        document.querySelector("h1").innerHTML = "PLAYER 2 WON"; 
+        win2 = true; 
+        // document.querySelector("h1").innerHTML = "PLAYER 2 WON"; 
     }
     if(rand1 == 3 && rand2 == 2){
-        document.querySelector("h1").innerHTML = "PLAYER 1 WON"; 
+        win1 = true; 
+        // document.querySelector("h1").innerHTML = "PLAYER 1 WON"; 
     }
+
+    if(win1 == true){
+        document.querySelector("h1").innerHTML = "PLAYER 1 WON"; 
+        count_one++; 
+    }
+    if(win2 == true){
+        document.querySelector("h1").innerHTML = "PLAYER 2 WON"; 
+        count_two++;
+    }
+
+    
+
+
 
 }
 
